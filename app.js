9e02793c4465bb93f416072951e63a1cd7b15170
Manuel Garcia <manueldev250799@gmail.com>
2020-12-21 21:26:30 -0500

    add: add files to create a small nodejs server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello world from nodejs')
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+   return res.send('hello world from aws cloud')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+   console.log("running server at port ", process.env.PORT || 3000)
+})
